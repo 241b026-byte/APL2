3a1771a461cf56dc12e1188bfc9765cee5697ff8
241B026 <241B026@juet.co.in>
2026-08-03 15:51:27 +0530

    solve problem with java

---
+++ Main.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("3x" + i + "=" + (3 * i));
+        }
+    }
+}
