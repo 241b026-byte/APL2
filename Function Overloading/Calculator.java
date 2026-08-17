bf8e4450c5af4e97f738d2465a1be2063f2a5868
241B026 <241B026@juet.co.in>
2026-08-17 15:33:46 +0530

    solved le2

---
+++ Function Overloading/Calculator.java
@@ -0,0 +1,17 @@
+class Calculator {
+
+    // Method with 2 integers
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method with 3 integers
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Method with 2 double values
+    double add(double a, double b) {
+        return a + b;
+    }
+}
